810460d17cef06aec586d85cf4356d58acfc1d38
Julia <bubnova.j.i@gmail.com>
2023-05-04 23:33:10 +0200

    Add horizontal droppable area for the columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { DragDropContext } from "@hello-pangea/dnd";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import styled from "styled-components";
 import { Column } from "./Column";
 
 // export function App() {
@@ -27,9 +28,23 @@ const initialData = {
       title: "To do",
       taskIds: ["task-1", "task-2", "task-3", "task-4"],
     },
+    "column-2": {
+      id: "column-2",
+      title: "In progress",
+      taskIds: [],
+    },
+    "column-3": {
+      id: "column-3",
+      title: "Done",
+      taskIds: [],
+    },
   },
-  columnOrder: ["column-1"],
+  columnOrder: ["column-1", "column-2", "column-3"],
 };
+
+const Container = styled.div`
+  display: flex;
+`;
 
 export class App extends React.Component {
   constructor(props) {
@@ -40,32 +55,75 @@ export class App extends React.Component {
   onDragEnd = (result) => {
     console.log("onDragEnd", result);
     document.body.style.color = "inherit";
-    const { destination, source, draggableId } = result;
+    const { destination, source, draggableId, type } = result;
 
     if (!destination) return;
 
     if (destination.droppableId === source.droppableId && destination.index === source.index)
       return;
 
-    const column = this.state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    if (type === "column") {
+      const newColumnOrder = Array.from(this.state.columnOrder);
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, draggableId);
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds,
-    };
+      const newState = {
+        ...this.state,
+        columnOrder: newColumnOrder,
+      };
 
-    const newState = {
-      ...this.state,
-      columns: {
-        ...this.state.columns,
-        [newColumn.id]: newColumn,
-      },
-    };
+      this.setState(newState);
+      return;
+    }
 
-    this.setState(newState);
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      this.setState(newState);
+    } else {
+      const startTaskIds = Array.from(start.taskIds);
+      startTaskIds.splice(source.index, 1);
+      const newStart = {
+        ...start,
+        taskIds: startTaskIds,
+      };
+
+      const finishTaskIds = Array.from(finish.taskIds);
+      finishTaskIds.splice(destination.index, 0, draggableId);
+      const newFinish = {
+        ...finish,
+        taskIds: finishTaskIds,
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newStart.id]: newStart,
+          [newFinish.id]: newFinish,
+        },
+      };
+
+      this.setState(newState);
+    }
   };
 
   onDragStart = (result) => {
@@ -87,12 +145,23 @@ export class App extends React.Component {
         onDragUpdate={this.onDragUpdate}
         onDragEnd={this.onDragEnd}
       >
-        {this.state.columnOrder.map((columnId) => {
-          const column = this.state.columns[columnId];
-          const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
+        <Droppable droppableId="all-columns" direction="horizontal" type="column">
+          {(provided, snapshot) => (
+            <Container
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              // isDraggingOver={snapshot.isDraggingOver}
+            >
+              {this.state.columnOrder.map((columnId, index) => {
+                const column = this.state.columns[columnId];
+                const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
 
-          return <Column key={columnId} column={column} tasks={tasks} />;
-        })}
+                return <Column key={columnId} column={column} tasks={tasks} index={index} />;
+              })}
+              {provided.placeholder}
+            </Container>
+          )}
+        </Droppable>
       </DragDropContext>
     );
   }
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import { Droppable } from "@hello-pangea/dnd";
+import { Draggable, Droppable } from "@hello-pangea/dnd";
 import Task from "./Task";
 
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
+  background-color: white;
   border-radius: 2px;
+  width: 220px;
+  display: flex;
+  flex-direction: column;
 `;
 const Title = styled.h3`
   padding: 8px;
@@ -14,40 +18,36 @@ const Title = styled.h3`
 const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
-  background-color: ${(props) => (props.isDraggingOver ? "skyblue" : "white")};
+  background-color: ${(props) => (props.isDraggingOver ? "skyblue" : "inherit")};
+  flex-grow: 1;
+  min-height: 100px;
 `;
 
-export class Column extends React.Component<{ tasks: any; column: any }> {
+export class Column extends React.Component<{ tasks: any; column: any; index: number }> {
   render() {
     const { tasks, column } = this.props;
     return (
-      <Container>
-        <Title>{column.title}</Title>
-        <Droppable droppableId={column.id}>
-          {(provided, snapshot) => (
-            <TaskList
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              isDraggingOver={snapshot.isDraggingOver}
-            >
-              {this.props.tasks.map((task, index) => (
-                <Task key={task.id} task={task} index={index} />
-              ))}
-              {provided.placeholder}
-            </TaskList>
-          )}
-        </Droppable>
-        {/* <Droppable droppableId={column.id}> */}
-        {/*  {(provided) => ( */}
-        {/*    <div innerRef={provided.innerRef} {...provided.droppableProps}> */}
-        {/*      {tasks.map((task, index) => ( */}
-        {/*        <Task task={task} index={index} /> */}
-        {/*      ))} */}
-        {/*      {provided.placeholder} */}
-        {/*    </div> */}
-        {/*  )} */}
-        {/* </Droppable> */}
-      </Container>
+      <Draggable draggableId={this.props.column.id} index={this.props.index}>
+        {(provided) => (
+          <Container {...provided.draggableProps} ref={provided.innerRef}>
+            <Title {...provided.dragHandleProps}>{column.title}</Title>
+            <Droppable droppableId={column.id}>
+              {(provided, snapshot) => (
+                <TaskList
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  isDraggingOver={snapshot.isDraggingOver}
+                >
+                  {this.props.tasks.map((task, index) => (
+                    <Task key={task.id} task={task} index={index} />
+                  ))}
+                  {provided.placeholder}
+                </TaskList>
+              )}
+            </Droppable>
+          </Container>
+        )}
+      </Draggable>
     );
   }
 }
