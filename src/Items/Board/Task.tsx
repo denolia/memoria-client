9e981295812a278796ebc63a9aecc8e800d1f411
Julia <bubnova.j.i@gmail.com>
2023-05-08 23:21:02 +0200

    Add edit button, fix override of values after editing

--- src/Items/Board/Task.tsx
+++ src/Items/Board/Task.tsx
@@ -1,10 +1,12 @@
 import { Draggable } from "@hello-pangea/dnd";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
 import { createTheme } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { useConfirm } from "material-ui-confirm";
 import React from "react";
+import { useNavigate } from "react-router";
 import { useItems } from "../state/ItemContext";
 import type { Item } from "../types";
 
@@ -17,6 +19,8 @@ interface Props {
 
 export default function Task({ order, taskId }: Props) {
   const { deleteItem, items } = useItems();
+  const navigate = useNavigate();
+
   const confirm = useConfirm();
 
   const task = items[taskId];
@@ -25,6 +29,10 @@ export default function Task({ order, taskId }: Props) {
       await confirm({ title: "Delete this task?", description: task.title });
       deleteItem(taskId);
     } catch {}
+  };
+
+  const onEditItem = () => {
+    navigate(`/edit/${taskId}`);
   };
 
   return task ? (
@@ -40,7 +48,10 @@ export default function Task({ order, taskId }: Props) {
             }}
           >
             <Typography>{task?.title}</Typography>
-            <DeleteOutlineIcon color="secondary" onClick={onDelete} />
+            <div>
+              <EditIcon color="secondary" onClick={onEditItem} />
+              <DeleteOutlineIcon color="secondary" onClick={onDelete} />
+            </div>
           </Paper>
         </div>
       )}
