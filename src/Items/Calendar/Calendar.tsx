b7da92e93dc4658fb5029f94678c94c33e805b77
Julia <bubnova.j.i@gmail.com>
2023-05-23 22:26:06 +0200

    Fix overflow

--- src/Items/Calendar/Calendar.tsx
+++ src/Items/Calendar/Calendar.tsx
@@ -1,58 +1,23 @@
-import type {
-  EventApi,
-  DateSelectArg,
-  EventClickArg,
-  EventContentArg,
-  EventInput,
-} from "@fullcalendar/core";
-import FullCalendar from "@fullcalendar/react";
+import type { DateSelectArg, EventApi, EventClickArg, EventContentArg } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import React, { useRef, useState } from "react";
 import type { IndexedItems, Item } from "../types";
 
 let eventGuid = 0;
-const todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD of today
 
 export function createEventId() {
   eventGuid += 1;
   return String(eventGuid);
 }
 
-export const INITIAL_EVENTS: EventInput[] = [
-  {
-    id: createEventId(),
-    title: "All-day event",
-    start: todayStr,
-  },
-  {
-    id: createEventId(),
-    title: "Timed event",
-    start: `${todayStr}T12:00:00`,
-  },
-];
-
-// export function Calendar() {
-//   return (
-//     <FullCalendar
-//       plugins={[dayGridPlugin]}
-//       initialView="dayGridMonth"
-//       events={[
-//         { title: "event 1", date: "2023-05-23" },
-//         { title: "event 2", date: "2023-05-26" },
-//       ]}
-//       eventContent={renderEventContent}
-//     />
-//   );
-// }
-
 function renderEventContent(eventContent: EventContentArg) {
   return (
-    <>
-      {/* <b>{eventContent.}</b> */}
-      <i style={{ overflow: "hidden", textOverflow: "ellipsis" }}>{eventContent.event.title}</i>
-    </>
+    <div style={{ overflow: "hidden", textOverflow: "ellipsis", padding: "4px 8px" }}>
+      {eventContent.event.title}
+    </div>
   );
 }
 
