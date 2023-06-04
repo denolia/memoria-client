279648dc373c1e825af3ab7318bdb65736c68a48
Julia <bubnova.j.i@gmail.com>
2023-06-04 20:34:56 +0200

    Add promo string

--- src/Auth/AuthContext.tsx
+++ src/Auth/AuthContext.tsx
@@ -11,7 +11,7 @@ interface AuthContext {
   userInitials: string;
   isLoggedIn: boolean;
   login: (email: string, password: string) => Promise<void>;
-  signup: (email: string, password: string) => Promise<void>;
+  signup: (email: string, password: string, promo: string) => Promise<void>;
   logout: () => void;
   currentSpace: Space | null;
   addSpace: (space: Space) => void;
@@ -92,8 +92,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     navigate("/");
   };
 
-  const signup = async (email: string, password: string) => {
-    const signupRes = await requestSignup(email, password);
+  const signup = async (email: string, password: string, promo: string) => {
+    const signupRes = await requestSignup(email, password, promo);
     if (signupRes === 200) {
       navigate("/login");
     }
--- src/Auth/state/requestSignup.ts
+++ src/Auth/state/requestSignup.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { API } from "../../environment";
 
-export async function requestSignup(username: string, password: string) {
+export async function requestSignup(username: string, password: string, promo: string) {
   try {
     const response = await axios.post(`${API}user/signup`, {
       username,
       password,
+      promo,
     });
     return response.status;
   } catch (e) {
