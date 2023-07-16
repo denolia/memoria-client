172aec43eb59e058a96ec3685d8bd1b0c1f00a73
Julia <bubnova.j.i@gmail.com>
2023-07-16 18:59:49 +0200

    Remove snackbar context, update copyright

--- src/Common/Copyright.tsx
+++ src/Common/Copyright.tsx
@@ -2,18 +2,16 @@ import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import React from "react";
 
-export function Copyright(props: any) {
+export function Copyright() {
   return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      sx={{ mt: 8, mb: 4 }}
-      {...props}
-    >
+    <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 8, mb: 4 }}>
       Copyright ©
       <Link color="inherit" href="https://juliabubnova.com/">
-        denolia&demoth
+        denolia
+      </Link>
+      &
+      <Link color="inherit" href="https://demoth.com/">
+        demoth
       </Link>{" "}
       {new Date().getFullYear()}
     </Typography>
